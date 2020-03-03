64b84e357574ca96528b4da144f6d7c9c08ae118
walter <walter@dipu.com>
2020-03-03 09:19:25 +0100

    #609

---
+++ Dipu.Excel/Range.cs
@@ -0,0 +1,17 @@
+﻿namespace Dipu.Excel
+{
+    public struct NamedRange
+    {
+        public string WorksheetName { get; set; }
+
+        public string Name { get; set; }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public int Rows { get; set; }
+
+        public int Columns { get; set; }
+    }
+}
